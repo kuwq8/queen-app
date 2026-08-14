0ae922f30f23b3d36a78e6ad9d2bfd463b433f73
Your Name <you@example.com>
2026-08-14 18:10:09 +0300

    fix: binaryTargets and explicit prisma connection logging

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -4,7 +4,13 @@ import { PrismaClient } from '@prisma/client';
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
   async onModuleInit() {
-    await this.$connect();
+    try {
+      await this.$connect();
+      console.log('Successfully connected to the database.');
+    } catch (error: any) {
+      console.error('FAILED TO CONNECT TO PRISMA. Error Message:', error.message);
+      console.error('Prisma Error Details:', error);
+    }
   }
 
   async onModuleDestroy() {
