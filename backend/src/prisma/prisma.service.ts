e99b4efbdf5b389de2e586e262f4a52f0d9500be
Your Name <you@example.com>
2026-08-14 19:16:46 +0300

    fix: hardcode database url to bypass Railway UI bug

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -3,6 +3,16 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  constructor() {
+    super({
+      datasources: {
+        db: {
+          url: 'postgresql://postgres.hamqmslzhlcnksdliipl:E3pcMy0bx2Ayupwq@aws-0-eu-central-1.pooler.supabase.com:6543/postgres?pgbouncer=true',
+        },
+      },
+    });
+  }
+
   async onModuleInit() {
     try {
       await this.$connect();
